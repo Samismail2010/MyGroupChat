d5509e5537cd73774bdb7ce878db1f7538b11089
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-03-26 00:58:06 -0500

    created api route

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -63,6 +63,34 @@ const thoughtController = {
         })
         .catch(err => res.status(400).json(err));
     },
+
+//created reactions
+addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.thoughtId}, {$push: { reactions: body } }, { new: true, runValidators: true})
+    .then(dbThoughtData => {
+        if(!dbThoughtData) {
+            res.status(404).json({ message: 'No thoughts with this id!'});
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.json(err))
+},
+removeReaction({ params }, res) {
+    Thought.findOneAndDelete(
+        { _id: params.thoughtId},
+        {$pull: { reactions: { reactionId: params.reactionId } } },
+        { new: true}
+    )
+    .then(dbThoughtData => {
+        if(!dbThoughtData) {
+            res.status(404).json ({message: "Reaction deleted"});
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.json(err))
 }
+};
 
 module.exports = thoughtController
